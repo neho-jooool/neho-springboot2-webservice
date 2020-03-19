bd28940755bd0ac81db7f4fa563da20acf7f6f05
hellosoboroo <hellosoboroo@gmail.com>
2020-03-19 23:03:04 +0900

    오류 발생 파일

--- src/main/java/com/neho/book/springboot/web/dto/PostsSaveRequestDto.java
+++ src/main/java/com/neho/book/springboot/web/dto/PostsSaveRequestDto.java
@@ -2,7 +2,11 @@ package com.neho.book.springboot.web.dto;
 
 import com.neho.book.springboot.domain.posts.Posts;
 import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
+@Getter
+@NoArgsConstructor
 public class PostsSaveRequestDto {
     private String title;
     private String content;
